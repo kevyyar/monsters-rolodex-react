c817799e254d218c218b419b9df275d552e81f52
Kevin Barreto <barretokevin@hotmail.com>
2022-03-22 16:00:57 -0600

    react basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchString: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState(() => {
+        return {
+          monsters: users
+        }
+      }))
+  }
+
+  handleSearchChange = (e) => {
+    console.log(e.target.value)
+    this.setState(() => {
+      return {
+        searchString: e.target.value
+      }
+    })
+  }
+
+  render() {
+    const { monsters, searchString } = this.state
+    const { handleSearchChange } = this
+
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchString)
+    })
+
+    return (
+      <div className="container">
+        <input type="text" placeholder='search monsters' onChange={handleSearchChange} />
+        {filteredMonsters.map(monster => {
+          return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
